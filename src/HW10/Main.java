ff29bc7f32e9a5925907175008250719f67d0618
skarapedulbuk <voronezh2@gmail.com>
2021-09-14 16:32:06 +0300

    HW10 Task2

--- src/HW10/Main.java
+++ src/HW10/Main.java
@@ -1,13 +1,10 @@
 package HW10;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 /* Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
    из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.
-TODO Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
+ Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
  В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать номер
  телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
   тогда при запросе такой фамилии должны выводиться все телефоны. Желательно не добавлять лишний функционал (дополнительные
@@ -21,11 +18,10 @@ public class Main {
         doTask2();
     }
 
-    public static int countEntries(ArrayList arrayList, String entry) {
+    public static int countEntries(List<String> arrayList, String entry) {
         int count = 0;
-        Iterator<String> iterator = arrayList.iterator();
-        while (iterator.hasNext()) {
-            if ((iterator.next()).equals(entry)) {
+        for (String currentWord : arrayList) {
+            if (currentWord.equals(entry)) {
                 count++;
             }
         }
@@ -39,16 +35,14 @@ public class Main {
     public static void doTask1() {
         System.out.println("* TASK 1 *");
         String[] array = createSomeStringArray();
-        ArrayList<String> arrayList = new ArrayList<>();
-        for (String word : array) {
-            arrayList.add(word);
-        }
+        List<String> arrayList = new ArrayList<>();
+        Collections.addAll(arrayList, array);
         System.out.println("Исходный массив: " + arrayList);
 
         HashSet<String> set = new HashSet<>(arrayList);
         System.out.println("Уникальные элементы массива: " + set);
 
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        Map<String, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < arrayList.size(); i++) {
             String currentWord = arrayList.get(i);
             hashMap.put(currentWord, countEntries(arrayList, currentWord));
@@ -60,5 +54,19 @@ public class Main {
     public static void doTask2() {
         System.out.println("* TASK 2 *");
 
+        PhoneBook phoneBook1 = new PhoneBook();
+        String target = "Бен Ладен";
+
+        phoneBook1.add("Путин", "+7495001");
+        phoneBook1.add("Лукашенко", "+375175");
+        phoneBook1.add("Зеленский", "+380504");
+        phoneBook1.add("Бен Ладен", "+9320");
+        phoneBook1.add("Бен Ладен", "+1212");
+        phoneBook1.add("Бен Ладен", "+966");
+        phoneBook1.add("Елизавета 2", "+445045");
+
+        System.out.println(phoneBook1);
+        System.out.printf("По запросу '%s' найдены следующие телефоны: ", target);
+        System.out.println(phoneBook1.get(target));
     }
 }
