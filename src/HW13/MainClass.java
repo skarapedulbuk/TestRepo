6dfcdb4d8d8df166479814de457e28983b8e8087
skarapedulbuk <voronezh2@gmail.com>
2021-09-19 18:47:01 +0300

    HW13 задача выполнена+++спорно

--- src/HW13/MainClass.java
+++ src/HW13/MainClass.java
@@ -17,32 +17,32 @@ public class MainClass {
     public static final int CARS_COUNT = 4;
     public static final Semaphore tunnelSemaphore = new Semaphore(CARS_COUNT / 2);
     public static final CountDownLatch finishCountDown = new CountDownLatch(CARS_COUNT);
-    public static final CyclicBarrier prepare = new CyclicBarrier(CARS_COUNT, new PreparationsComplete());
     public static Lock finishLock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(tunnelSemaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(finishLock, finishCountDown, prepare, race, 20 + (int) (Math.random() * 10));
-        }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        createRandomCompetition(cars);
         startTheRace(cars);
         finishCountDown.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 
-    public static void startTheRace(Car[] cars) throws InterruptedException {
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
-        }
-    }
-}
+    public static void createRandomCompetition(Car[] cars) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!! (Создание всей инфраструктуры)");
+        final CyclicBarrier prepare = new CyclicBarrier(CARS_COUNT, () -> System.out.println("Подготовка всех участников завершена"));
 
-class PreparationsComplete implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Подготовка всех участников завершена");
+        Race race = new Race(new Road(60), new Tunnel(tunnelSemaphore), new Road(40));
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(finishLock, finishCountDown, prepare, race, 20 + (int) (Math.random() * 10));
+        }
+   //     return cars;
+    }
+
+    public static void startTheRace(Car[] cars) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
     }
 }
